7f6a83ec2bdaf3c9d14544dd2006fad63b3bde57
Chandramouly40 <chandramouly64@gmail.com>
2025-07-28 00:13:11 +0530

    Add files via upload

---
+++ WEEK 6 scorecalculatorapp/src/App.js
@@ -0,0 +1,15 @@
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+  return (
+    <div>
+    <CalculateScore
+    Name={"Steeve"}
+    total={284}
+    goal={3} />
+    </div>
+
+  );  
+}
+
+export default App;
